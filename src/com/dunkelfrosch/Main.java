2f8299ae2cacac43c6ba705c4d4a61e3bfde244f
Patrick Paechnatz <patrick.paechnatz@t-systems-mms.com>
2019-01-31 07:51:23 +0100

    first commit

---
+++ src/com/dunkelfrosch/Main.java
@@ -0,0 +1,23 @@
+package com.dunkelfrosch;
+
+import java.util.Arrays;
+
+class BubbleSortImproved {
+
+    public static void main(String args[]) {
+
+        String[] metaDataNames = new String[] { "John", "Thomas", "Samuel", "Chris", "Daniel", "Joey", "David", "Joshua", "Michael", "John", "Andrew", "Peter", "Franklin" };
+        int[] metaDataNumbers = new int[] { 43, 41, 61, 72, 83, 109, 9, 23, 34, 18, 16, 19, 12, 122, 132, 111 };
+
+        // bubbleSortNormal(metaDataNumbers);
+        // bubbleSortImproved(metaDataNames);
+    }
+
+    private static void bubbleSortNormal(int[] numbers) { }
+
+    private static void bubbleSortImproved(String[] names) {}
+
+    private static void swapNumbers(int[] numbers, int fromIdx, int toIdx) {}
+
+    private static void swapString(String[] names, int fromIdx, int toIdx) {}
+}
